2f4313dbfdc08e0e6700605ae397faf419be4353
droll-demo-7630 <danjamrhi@hotmail.com>
2021-01-05 20:29:51 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
-def meal_choice(veg1, veg2, protein = "tofu")
+def meal_choice(veg1, veg2, protein = 'tofu')
   puts "What a nutrituious meal!"
-  "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
